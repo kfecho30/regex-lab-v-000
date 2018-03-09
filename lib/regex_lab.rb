3cd22ec4d8c5268fccdee6659ca97484a1240568
Kelsey Fecho <kfecho30@gmail.com>
2018-03-09 03:47:18 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\W+un/)
+  text.scan(/\b[u][n].*[i][n][g]\b/)
 end
 
 def words_five_letters_long(text)
