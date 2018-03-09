ca9402e1764408925355aca9da1994c562fdf635
Kelsey Fecho <kfecho30@gmail.com>
2018-03-09 03:41:20 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,17 +3,17 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\W+un/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\S{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z][$.?!]/)
 end
 
 def valid_phone_number?(phone)
-
+  text.match(/(\d+)(\d+(\d+))/)
 end
